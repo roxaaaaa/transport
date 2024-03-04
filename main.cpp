95159da66ba48ee67120e9e128a706aa4cc3a063
Юля Бабій <julia.babiy2006@gmail.com>
2024-03-04 11:33:56 +0200

    Create main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
-#include "iostream"
-int main(){
+#include "mainclass.h"
 
-    return 0;
+int main() {
+	int countC = 0, countB = 0, countT = 0;
+
+	Car* cars = setCar("text.txt", countC);
+	Bike* bikes = setBike("text.txt", countB);
+	Taxi* taxes = setTaxi("text.txt", countT);
+
+	displayCar("text.txt", cars, countC);
+	displayBike("text.txt", bikes, countB);
+	displayTaxi("text.txt", taxes, countT);
+
+	delete[] cars, bikes, taxes;
+
+	return 0;
 }
